278ec7bad2466d45d4a597d5515ed2546d0a79fa
qinzhihao <775051675@qq.com>
2018-11-24 13:58:34 +0800

    first commit

---
+++ Travel/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+    <div class="border-1px"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .border-1px:after{
+    border-color: blue;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+</style>
